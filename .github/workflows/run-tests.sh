863e2d9075853b13126ca3d543cf607a615cf9f1
Alexander Zhukov <zhukovap@ornl.gov>
2021-05-20 22:50:00 -0400

    Putting tests in separate files

--- .github/workflows/run-tests.sh
+++ .github/workflows/run-tests.sh
@@ -1,3 +1,4 @@
-source setupEnvironment.sh
+CUR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $CUR_DIR/../../setupEnvironment.sh
 cd examples/AccLattice_Tests
 ./START.sh lattice_test.py 2
