e9ca176769acda9d64c0d5a3ce6fe70a80726332
Alexander Zhukov <zhukovap@ornl.gov>
2021-05-21 00:13:58 -0400

    Added ubuntu image 20.04

--- .github/workflows/run-tests.sh
+++ .github/workflows/run-tests.sh
@@ -1,4 +1,3 @@
-CUR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-source $CUR_DIR/../../setupEnvironment.sh
+. setupEnvironment.sh
 cd examples/AccLattice_Tests
 ./START.sh lattice_test.py 2
